3cfe2c0c5037a78754c68bc22cff6a32815af23f
Dell3421 <dumtk1@gmail.com>
2017-06-18 22:06:08 +0700

    Add Page Splash

--- app/src/main/java/com/example/dell_3421/tourism_location/SplashActivity.java
+++ app/src/main/java/com/example/dell_3421/tourism_location/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.dell_3421.tourism_location;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,13 +11,42 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler;
+    private Runnable runnable;
+    long delay_time;
+    long time = 3000L;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_splash);
         getSupportActionBar().hide();
-        Intent go  = new Intent(SplashActivity.this,MainActivity.class);
-        startActivity(go);
-        finish();
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent go  = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(go);
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        delay_time = time;
+
+        handler.postDelayed(runnable,delay_time);
+        time = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delay_time - (System.currentTimeMillis() - time);
     }
 }
