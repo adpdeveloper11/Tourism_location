543e4875fc87c0092b53fb035503f76aa1be9aa0
Dell3421 <dumtk1@gmail.com>
2017-06-18 15:58:56 +0700

    init1

---
+++ app/src/main/java/com/example/dell_3421/tourism_location/Tab_Edit_Profile.java
@@ -0,0 +1,22 @@
+package com.example.dell_3421.tourism_location;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Dell3421 on 6/17/2017.
+ */
+
+public class Tab_Edit_Profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_profile);
+        getSupportActionBar().hide();
+
+
+    }
+}
